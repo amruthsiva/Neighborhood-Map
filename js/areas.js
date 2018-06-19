b558c14b219f6b9d1243d495fdf46512d4c74ca3
gudivada amrita siva kumar <38848739+amruthsiva@users.noreply.github.com>
2018-06-19 11:06:31 +0530

    Add files via upload

---
+++ js/areas.js
@@ -0,0 +1,68 @@
+var myLocations = [
+    {
+        title: 'Ramakrishna Beach',
+        lat: 17.7138467,
+        lng: 83.3158063,
+        type: 'Beach'
+    },
+    {
+        title: 'INS Kursura Submarine Museum',
+        lat: 17.716142,
+        lng: 83.3272829,
+        type: 'Museum'
+    },
+    {
+        title: 'Andhra University',
+        lat: 17.7225768,
+        lng: 83.3127826,
+        type:  'University'
+    },
+    {
+        title: 'Kailasagiri',
+        lat: 17.7465202,
+        lng: 83.3367637,
+        type:  'Park'
+    },
+    {
+        title: 'Varun Inox',
+        lat: 17.7106345,
+        lng: 83.3137141,
+        type: 'Theatre'
+    },
+    {
+        title: 'Novotel',
+        lat: 17.7107022,
+        lng: 83.3137003,
+        type: 'Hotel'
+    },
+    {
+        title: 'Dr YSR ACA VDCA Cricket Stadium',
+        lat: 17.7644714,
+        lng: 83.2924153,
+        type: 'Stadium'
+    },
+    {
+        title: 'Visakhapatnam International Airport',
+        lat: 17.7217786,
+        lng: 83.22526,
+        type: 'Airport'
+    },
+    {
+        title: 'Port Area',
+        lat: 17.6958263,
+        lng: 83.2864957,
+        type: 'Harbour'
+    },
+    {
+        title: 'KFC',
+        lat: 17.695823,
+        lng: 83.2602307,
+        type: 'Restaurant'
+    },
+    {
+        title: 'CMR Central',
+        lat: 17.734073,
+        lng: 83.315904,
+        type: 'Shopping Mall'
+    }
+]
